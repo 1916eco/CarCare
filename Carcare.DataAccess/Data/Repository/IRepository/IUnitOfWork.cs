9a88b82146240c99c8dd17ea5effe1854c90ff8a
BuildTools <unconfigured@null.spigotmc.org>
2020-04-01 15:17:05 +0100

    SetupCategories Repositories Final

---
+++ Carcare.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Carcare.DataAccess.Data.Repository.IRepository
+{
+    interface IUnitOfWork :IDisposable
+    {
+        ICategoryRepository Category { get; }
+        void Save();
+    }
+}
